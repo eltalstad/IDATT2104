dad1f57cf72b0772aa077a0c52ff83a9ce4d5450
eltalstad <eleertalstad@gmail.com>
2023-03-01 21:23:30 +0100

    Initialized oving5

---
+++ oving5/backend/src/test/java/stud/ntnu/idatt2104/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package stud.ntnu.idatt2104.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
