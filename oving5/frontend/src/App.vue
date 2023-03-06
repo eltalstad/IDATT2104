81866bd4bdbada61105f96b3b1d2ee74d28bc250
eltalstad <eleertalstad@gmail.com>
2023-03-06 20:28:27 +0100

    small changes

---
+++ oving5/frontend/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <CompilerComponent />
+  </div>
+</template>
+
+<script>
+import CompilerComponent from "@/components/CompilerComponent.vue";
+
+export default {
+  name: 'App',
+  components: {
+    CompilerComponent,
+  }
+}
+</script>
+
+<style>
+
+</style>
